075cfd08456665bf81b8de898d15ac0055c16d96
Lyuwen Fu <fulvwen@gmail.com>
2018-05-02 13:51:25 -0400

    more test

--- code.py
+++ code.py
@@ -3,3 +3,7 @@ import sys, os
 import numpy as np
 
 print "Hello World"
+
+from test_module import Test
+
+t = Test()
---
+++ pylib/test_module.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import sys, os
+import numpy as np
+
+class Test(object):
+  def __init__(self):
+    print "1"
